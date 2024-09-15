3fe62b52c0637c6a2a1276f4c218aa020700079d
Your Name <you@example.com>
2024-09-15 20:30:19 +0300

    last of card

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -15,12 +15,12 @@ const BookCard = ({ key, title, author, price, imageUrl }) => {
         <img src={imageUrl} alt={title} className="w-40 mt-6 h-48 mx-auto" />
       </div>
 
-      <div className="card-body flex-grow-0 ps-8 bodyCard">
-        <p className="text-red-600 uppercase mb-0 pb-0">Paperback</p>
+      <div className="card-body flex-grow-0 p-8 bodyCard ms-3">
+        <p className="text-red-600 uppercase mb-0 pb-0 ">Paperback</p>
         <h3 className="card-title text-xl font-bold mt-0 pt-0">{title}</h3>
         <p className="text-gray-700">{author}</p>
         <p className="text-xl font-sans font-bold">${price} </p>
-        <div className="card-title cartDev flex justify-between sm:text-md bg-white">
+        <div className="card-title cartDev flex justify-between sm:text-md">
           <span className="font-semibold ">Add to Cart</span>
           <FaRegHeart className="cursor-pointer iconBody" />
         </div>
