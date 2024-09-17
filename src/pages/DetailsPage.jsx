ab66284b904b8e727284c1e026db3397ed313a23
Your Name <you@example.com>
2024-09-17 23:01:30 +0300

    merging of fetch data and loader of home page

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -15,7 +15,9 @@ function DetailsPage() {
 
   const fetchBook = async (id) => {
     try {
-      const res = await axios.get(`http://localhost:5000/books/${id}`);
+      const res = await axios.get(
+        `https://book-store-backend-sigma-one.vercel.app/book/${id}`
+      );
       console.log(res.data);
 
       setBook(res.data);
@@ -54,28 +56,22 @@ function DetailsPage() {
 
           <div className="flex flex-col gap-7 md:col-span-2">
             <div className="flex flex-col gap-4">
-              <h1 className="font-medium text-3xl max-w-xl">
-                The Lost Colony (The Long Winter Trilogy Book 3)
-                {/* {book.title} */}
-              </h1>
-              <p className="text-md max-w-xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut Excepteur sint occaecat.
-                {/* {book.description} */}
-              </p>
+              <h1 className="font-medium text-3xl max-w-xl">{book.title}</h1>
+              <p className="text-md max-w-xl">{book.description}</p>
             </div>
 
             <div className="flex flex-col gap-4 md:flex-row md:items-center">
               <a className="btn btn-outline rounded-none px-8"
-                href='https://storage.googleapis.com/iti-final-grad-proj.appspot.com/books/Orwell-1949_1984.pdf?GoogleAccessId=firebase-adminsdk-kwfua%40iti-final-grad-proj.iam.gserviceaccount.com&Expires=16447010400&Signature=NtvtqE79peU%2FklGgQiY%2FRPSlBpu%2F%2Bv2oUNLz%2BawKBpMtnOWtsEDd32BO7hKtwcn7cJriNu2f8jYwZfJlHKtpWlIkC%2BjOZRA4emKro2Lbygv09gW6pUnV4w1lbGMU5B5gUWwQihnyZbqyWBEUBdJ3%2FK5P6qGtvjnQ7kWwdtjWJxOrihZqtTP8U5H0CynxD%2BtcQmrmBlWG7yiVSx5KQFPqlvyfp7MCvFK5PqnuIqAOx4vphr9QF9pgf%2FE8w1%2FRQo3sjxm6CimU0ASwHQewZmDtLwa0DGB4uiBPtdgD4LBfN%2FbwOm9ncejlK5IV3WvWwBnsxyYvBIlArrrT6jESDPS2MQ%3D%3D'
+                href={book.sourcePath}
                 target="_blank" rel="noreferrer">
                 View
               </a>
 
-              <a className="btn btn-neutral rounded-none px-8 text-white"
-                href='https://storage.googleapis.com/iti-final-grad-proj.appspot.com/books/Orwell-1949_1984.pdf?GoogleAccessId=firebase-adminsdk-kwfua%40iti-final-grad-proj.iam.gserviceaccount.com&Expires=16447010400&Signature=NtvtqE79peU%2FklGgQiY%2FRPSlBpu%2F%2Bv2oUNLz%2BawKBpMtnOWtsEDd32BO7hKtwcn7cJriNu2f8jYwZfJlHKtpWlIkC%2BjOZRA4emKro2Lbygv09gW6pUnV4w1lbGMU5B5gUWwQihnyZbqyWBEUBdJ3%2FK5P6qGtvjnQ7kWwdtjWJxOrihZqtTP8U5H0CynxD%2BtcQmrmBlWG7yiVSx5KQFPqlvyfp7MCvFK5PqnuIqAOx4vphr9QF9pgf%2FE8w1%2FRQo3sjxm6CimU0ASwHQewZmDtLwa0DGB4uiBPtdgD4LBfN%2FbwOm9ncejlK5IV3WvWwBnsxyYvBIlArrrT6jESDPS2MQ%3D%3D'
-                download="Orwell-1949_1984.pdf">
+              <a
+                href={book.sourcePath}
+                download
+                className="btn btn-neutral rounded-none px-8"
+              >
                 Download
               </a>
 
