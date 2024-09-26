ca2d7038f061ad0b2d84a34855d9ff208460a7bd
safaasamir <safaasamirsherif@gmail.com>
2024-09-27 00:37:05 +0300

    edit

--- src/components/LibraryComponent.jsx
+++ src/components/LibraryComponent.jsx
@@ -36,6 +36,7 @@ export default function LibraryComponent() {
   }, []);
 
   if (loading) {
+    
     return (
       <div className="flex justify-center items-center relative top-0 left-0 h-[50vh] w-fit my-[6.75rem] mx-auto ">
         <img src="/loader.gif" alt="Loading..." className="w-full h-full" />
