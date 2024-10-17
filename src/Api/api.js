f55e11d757e8fa474241f3a2f2f7d8480e8dbec5
Your Name <you@example.com>
2024-10-17 22:24:52 +0300

    merge of addtocart fix branch

--- src/Api/api.js
+++ src/Api/api.js
@@ -3,4 +3,4 @@ const api =axios.create({
     baseURL:"https://book-store-backend-sigma-one.vercel.app/users/"
 })
 
-export const googleAuth =(code)=>api.get(`/google?code=${code}`)
+export const googleAuth =(code)=>api.get(`/google?code=${code}`) 
