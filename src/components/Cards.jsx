a25f2e38f7010f6ac950a71f2db3f36521ed4be1
Smah-amen <samahamen20001@gmail.com>
2024-10-22 17:31:00 +0300

    desigen card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,36 +4,47 @@ import { FaTrash } from 'react-icons/fa';
 
 function Cards({ card, onDelete }) {
   return (
-    <div className="container mx-auto p-4 border-b bg-white shadow-md rounded-lg">
+    <div className="container mx-auto p-4 border-b bg-white  rounded-lg">
       
-      <div className="flex justify-end">
+      {/* <div className="flex justify-end">
         <button
           className="text-blue-950 hover:text-red-700 transition"
           onClick={() => onDelete(card._id)}
         >
          <FaTrash size={20} />
         </button>
-      </div>
+      </div> */}
 
-   
-      <div className="flex flex-col items-center py-4  ">
-        
+      <div className="relative flex flex-col items-center py-4">
         
         <img
-          className=" object-cover mb-4 h-full w-36 object-center "
+          className="object-cover h-auto w-96 rounded-lg"
           src="/profile/credit-card.png"
-          alt="Wallet"
+          alt="Credit Card"
         />
 
- 
-        <div className="text-center">
-          <p className="font-poppins font-semibold text-brand-black mb-1">
-            {card.cardholderName}
-          </p>
-          <p className="font-poppins text-brand-black">
-            {maskNumbers(card.cardNumber)}
-          </p>
-        </div>
+
+             {/* <div className="flex justify-end"> */}
+            
+        <button
+          className="text-white hover:text-red-700 transition absolute top-5 right-2"
+          onClick={() => onDelete(card._id)}
+        >
+         <FaTrash size={15} />
+        </button>
+      {/* </div> */}  
+        <p className="absolute bottom-8 left-8 text-white text-xl font-semibold">
+          {card.cardholderName}
+        </p>
+
+        <p className="absolute bottom-20 left-8 text-white text-lg font-mono tracking-widest">
+          {maskNumbers(card.cardNumber)}
+        </p>
+        
+        <p className="absolute bottom-14 items-center text-white text-sm font-semibold">
+        {card.expiryDate}
+        </p>
+
       </div>
       
     </div>
@@ -41,3 +52,4 @@ function Cards({ card, onDelete }) {
 }
 
 export default Cards;
+
